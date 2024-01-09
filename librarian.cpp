186acc9ecffeb5e342e5d7f211d3a7712772d53c
Arfanul Shouid <arfanulshouid3999@gmail.com>
2024-01-09 19:22:36 +0000

    I am adding my classes

---
+++ librarian.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "librarian.h"
+using namespace std;
+
+
+void message::printMessage(){
+    cout << "Makefile Example\n";
+}
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "librarian.h"
+#include "member.h"
+#include "person.h"
+#include "book.h"
+
+
+using namespace std;
+
+int main()
+
+{
+    message m;
+
+    m.printMessage();
+
+    return 0;
+
+
+
+
+
+}
